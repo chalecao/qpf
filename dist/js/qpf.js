9bd10b14ff83bb0dda5b5a26061cf9247db24414
lang <bm2736892@gmail.com>
2014-06-06 09:47:51 +0800

    Spinner improvement

--- dist/js/qpf.js
+++ dist/js/qpf.js
@@ -3965,7 +3965,7 @@ define('qpf/meta/Spinner',['require','./Meta','knockout','$','_','../helper/Drag
 	var Spinner = Meta.derive(function() {
 		var ret = {
 			step : ko.observable(1),
-			valueUpdate : "afterkeydown", //"keypress" "keyup" "afterkeydown"
+			valueUpdate : "", //"keypress" "keyup" "afterkeydown" "input"
 			precision : ko.observable(2),
 			min : ko.observable(null),
 			max : ko.observable(null),
@@ -4023,18 +4023,19 @@ define('qpf/meta/Spinner',['require','./Meta','knockout','$','_','../helper/Drag
 			this._$value = this.$el.find(".qpf-spinner-value")
 			// numeric input only
 			this._$value.keydown(function(event) {
-				// Allow: backspace, delete, tab, escape and dot
-				if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 190 ||
+				var keyCode = event.keyCode;
+				// Allow: backspace, delete, tab, escape, minus, dot
+				if (keyCode == 46 || keyCode == 8 || keyCode == 9 || keyCode == 27 || keyCode == 190 || keyCode == 189 ||
 					 // Allow: Ctrl+A
-					(event.keyCode == 65 && event.ctrlKey === true) || 
+					(keyCode == 65 && event.ctrlKey === true) || 
 					// Allow: home, end, left, right
-					(event.keyCode >= 35 && event.keyCode <= 39)) {
+					(keyCode >= 35 && keyCode <= 39)) {
 					// let it happen, don't do anything
 					return;
 				}
 				else {
 					// Ensure that it is a number and stop the keypress
-					if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+					if (event.shiftKey || (keyCode < 48 || keyCode > 57) && (keyCode < 96 || keyCode > 105)) {
 						event.preventDefault(); 
 					}
 		        }
